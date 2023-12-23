02408b0e0f41aa0422272b1ecc92f0a63f309d1d
Kevin Németh <nkevin@outlook.hu>
2023-12-23 23:10:14 +0100

    Finish try_from_into

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -27,8 +27,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
 // integers, an array of three integers, and a slice of integers.
@@ -37,10 +35,23 @@ enum IntoColorError {
 // time, but the slice implementation needs to check the slice length! Also note
 // that correct RGB color values must be integers in the 0..=255 range.
 
+fn convert_to_valid_color(number: i16) -> Result<u8, IntoColorError> {
+    match u8::try_from(number) {
+        Ok(val) => Ok(val),
+        Err(_) => Err(IntoColorError::IntConversion),
+    }
+}
+
 // Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let (red, green, blue) = tuple;
+        Ok(Color {
+            red: convert_to_valid_color(red)?,
+            green: convert_to_valid_color(green)?,
+            blue: convert_to_valid_color(blue)?,
+        })
     }
 }
 
@@ -48,6 +59,11 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        Ok(Color {
+            red: convert_to_valid_color(arr[0])?,
+            green: convert_to_valid_color(arr[1])?,
+            blue: convert_to_valid_color(arr[2])?,
+        })
     }
 }
 
@@ -55,6 +71,14 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+        Ok(Color {
+            red: convert_to_valid_color(slice[0])?,
+            green: convert_to_valid_color(slice[1])?,
+            blue: convert_to_valid_color(slice[2])?,
+        })
     }
 }
 
